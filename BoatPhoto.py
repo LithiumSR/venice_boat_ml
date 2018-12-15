cd6e0a5799c2e66de1a4a849f83e57d8b0f84e1a
Leonardo Sarra <leonardosarra@outlook.com>
2018-12-15 22:24:01 +0100

    Add variable that to indicate the source of the file

--- BoatPhoto.py
+++ BoatPhoto.py
@@ -1,10 +1,11 @@
 import os
 class BoatPhoto:
 
-    def __init__(self, boatType, features, filename):
+    def __init__(self, boatType, features, filename, partOfTestingSet=False):
         self.boatType = boatType
         self.features = features
         self.filename = filename
+        self.partOfTestingSet = partOfTestingSet
 
     def __repr__(self):
         return "(Type:{},filename:{}, features:{})".format(self.boatType, os.path.basename(os.path.normpath(self.filename)), self.features.shape)
