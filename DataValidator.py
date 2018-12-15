e632d085824b644899fff0bbbb1ca53089b95c4a
Leonardo Sarra <leonardosarra@outlook.com>
2018-12-15 22:47:59 +0100

    Support to use exclusively the testing ARGOS set

--- DataValidator.py
+++ DataValidator.py
@@ -11,12 +11,24 @@ from DataLearner import BoatLearner
 
 
 class DataValidator:
+
     def __init__(self, set, split, mode, vstype="Water", kernel="linear"):
         self.set = set
         self.split = split
         self.mode = mode
         self.vstype = vstype.strip()
         self.kernel = kernel
+
+    def defaultvalidate(self):
+        from DataLoader import Mode
+        testing = list(filter(lambda x: x.partOfTestingSet == True, self.set))
+        training = list(filter(lambda x: x.partOfTestingSet == False, self.set))
+        print(testing)
+        print(training)
+        if self.mode == Mode.detection:
+            self._defaultvalidatedetection(testing, training)
+        else:
+            self._defaultvalidateclassification(testing, training)
 
     def kcrossvalidate(self):
         from DataLoader import Mode
@@ -25,12 +37,47 @@ class DataValidator:
         else:
             self._kcrossvalidateclassification()
 
+    def _defaultvalidatedetection(self, testing, training):
+        print("Validating using default testing set...")
+        truepositive, truenegative, falsepositive, falsenegative = [0] * 4
+        learner = BoatLearner(training, self.kernel)
+        learner.learn()
+        result = self._validatedetection(testing, learner)
+        print(result)
+        truepositive += result[0]
+        truenegative += result[1]
+        falsepositive += result[2]
+        falsenegative += result[3]
+        print("Precision: {0:.2f}".format((truepositive / (truepositive + falsepositive))))
+        print("Recall: {0:.2f}".format((truepositive / (truepositive + falsenegative))))
+        print("False positive rate: {0:.2f}".format((falsepositive / (falsepositive + truenegative))))
+        print("Accuracy: {0:.2f}".format(
+            (truenegative + truepositive) / (falsepositive + truenegative + truepositive + falsenegative)))
+
+    def _defaultvalidateclassification(self, testing, training):
+        print("Validating using default testing set...")
+        order = set()
+        for elem in self.set:
+            order.add(elem.boatType)
+        order = list(order)
+        print("Validating using kcross...")
+        learner = BoatLearner(training, self.kernel)
+        learner.learn()
+        matrix = self._validateclassification(testing, learner, order)
+        matrix = matrix.round()
+        np.set_printoptions(suppress=True)
+        df = pd.DataFrame(matrix, index=order, columns=order)
+        plt.figure(figsize=(30, 14))
+        sn.set(font_scale=1.5)
+        sn.heatmap(df, annot=True, fmt='g')
+        plt.show()
+
     def _kcrossvalidatedetection(self):
         kf = KFold(n_splits=self.split, shuffle=True)
         kf.get_n_splits(self.set)
         truepositive, truenegative, falsepositive, falsenegative = [0] * 4
         print("Validating using kcross...")
-        for train_index, test_index in tqdm.tqdm(kf.split(self.set)):
+        for train_index, test_index in kf.split(self.set):
             X_train, X_test = [], []
             for index in train_index:
                 X_train.append(self.set[index])
@@ -38,7 +85,7 @@ class DataValidator:
                 X_test.append(self.set[index])
             learner = BoatLearner(X_train, self.kernel)
             learner.learn()
-            result = self._validatedetection(learner)
+            result = self._validatedetection(X_test, learner)
             print(result)
             truepositive += result[0]
             truenegative += result[1]
@@ -54,12 +101,14 @@ class DataValidator:
         print("Accuracy: {0:.2f}".format(
             (truenegative + truepositive) / (falsepositive + truenegative + truepositive + falsenegative)))
 
-    def _validatedetection(self, learner):
+    def _validatedetection(self, testing, learner):
         truepositive, truenegative, falsepositive, falsenegative = [0] * 4
-        for elem in self.set:
+        for elem in testing:
             tmp = [elem.features.flatten()]
             prediction = learner.classifier.predict(tmp)
             # print(prediction,"but its type is: ", elem.boatType)
+            prediction = prediction[0].strip()
+            print(prediction, elem.boatType, self.vstype)
             if prediction == elem.boatType:
                 if prediction == self.vstype:
                     truepositive += 1
@@ -74,10 +123,10 @@ class DataValidator:
                     falsenegative += 1
         return truepositive, truenegative, falsepositive, falsenegative
 
-    def _validateclassification(self, learner, order):
+    def _validateclassification(self, testing, learner, order):
         y_true = []
         y_prediction = []
-        for elem in self.set:
+        for elem in testing:
             tmp = [elem.features.flatten()]
             prediction = learner.classifier.predict(tmp)
             y_true.append(elem.boatType)
@@ -86,7 +135,7 @@ class DataValidator:
         return matrix
 
     def _kcrossvalidateclassification(self):
-        kf = KFold(n_splits=self.split)
+        kf = KFold(n_splits=self.split, shuffle=True)
         kf.get_n_splits(self.set)
         matrix = None
         order = set()
@@ -102,12 +151,11 @@ class DataValidator:
                 X_test.append(self.set[index])
             learner = BoatLearner(X_train, self.kernel)
             learner.learn()
-            result = self._validateclassification(learner, order)
+            result = self._validateclassification(X_test, learner, order)
             if matrix is None:
                 matrix = result
             else:
                 matrix += result
-            break
         matrix = matrix / self.split
         matrix = matrix.round()
         np.set_printoptions(suppress=True)
